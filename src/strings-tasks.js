67459d8ec5bed4ee9d3bacee7a1a7128271f3095
KirillGenin <332211op@gmail.com>
2023-12-21 20:04:27 +0300

    feat: implemented countVowels function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -300,8 +300,14 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = 'aeiouyAEIOUY';
+
+  const count = str
+    .split('')
+    .reduce((acc, char) => (vowels.includes(char) ? acc + 1 : acc), 0);
+
+  return count;
 }
 
 /**
