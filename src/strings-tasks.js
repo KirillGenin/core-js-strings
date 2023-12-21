b1a65887d5cd7877181f5c8105cea747ea01ec28
KirillGenin <332211op@gmail.com>
2023-12-21 20:17:02 +0300

    feat: implemented isPalindrome function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -323,8 +323,11 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const format = str.toLowerCase().replace(/[\W_]/g, '');
+  const reverse = format.split('').reverse().join('');
+
+  return format === reverse;
 }
 
 /**
