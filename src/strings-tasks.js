2b1d5690c083a9c3765dbd56e2d715ef8c4d92a5
KirillGenin <332211op@gmail.com>
2023-12-21 19:36:59 +0300

    feat: implemented formatTime function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -235,8 +235,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const formatTimeNow = (time) => time.toString().padStart(2, '0');
+
+  return `${formatTimeNow(minutes)}:${formatTimeNow(seconds)}`;
 }
 
 /**
